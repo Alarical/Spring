a68ab99ff66fd011367eb0240b5adddfc67763b0
Alarical <zrh2014@163.com>
2020-10-09 22:33:48 +0800

    update

--- itheima_spring_jdbc/src/test/java/com/itheima/test/JdbcCurdTest.java
+++ itheima_spring_jdbc/src/test/java/com/itheima/test/JdbcCurdTest.java
@@ -1,11 +1,15 @@
 package com.itheima.test;
 
+import com.itheima.domain.Account;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:applicationContext.xml")
@@ -17,4 +21,11 @@ public class JdbcCurdTest {
     public void updateTest(){
         jdbcTemplate.update("update test_mysql set money=? where name=?", 10000,"tom");
     }
+
+    @Test
+    public void getQueryAll(){
+        List<Account> accountList = jdbcTemplate.query("select * from test_mysql", new BeanPropertyRowMapper<Account>(Account.class));
+        System.out.println(accountList);
+    }
+
 }
